236ce0684c68f113fc42f81c2b5ce7f60e517aaf
cbl1995 <cuibilin@163.com>
2019-01-30 14:32:10 +0800

    create Demo class

--- src/a.java
+++ src/a.java
@@ -2,6 +2,7 @@ public class a {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello github");
+        System.out.println("Hello world");
     }
 
 
